07b85d55270f216ad7bd893d1c47fc33d06f3731
Byanca De la fuente <byancadlf@gmail.com>
2019-12-21 21:22:22 -0600

    up date

--- bamazon.js
+++ bamazon.js
@@ -37,6 +37,7 @@ let table = function() {
       );
     }
     start(res);
+    // console.log(res);
   });
 };
 
@@ -48,24 +49,47 @@ let start = function(res) {
       message: "What item would you like to purhcase? (enter product name)"
     })
     .then(function(answer) {
-      // console.log(answer);
+      // console.log(res.length);
       for (let i = 0; i < res.length; i++) {
         if (answer.item == res[i].product_name) {
           inquirer
             .prompt({
               name: "quant",
               type: "input",
-              message: "How many of these would you like to buy?"
-              // validate: function(value) {
-              //   if (isNaN(value) == false) {
-              //     return true;
-              //   } else {
-              //     return false;
-              //   }
-              // }
+              message: "How many units of these would you like to buy?",
+              validate: function(value) {
+                if (isNaN(value) == false) {
+                  return true;
+                } else {
+                  return false;
+                }
+              }
             })
             .then(function(answer) {
-              console.log(answer);
+              let price = 0;
+              if (answer.quant > 0 && res[i].stock_quantity >= answer.quant) {
+                res[i].stock_quantity = res[i].stock_quantity - answer.quant;
+                console.log(
+                  "You purchased " + answer.quant + " your total will be..."
+                );
+                price = answer.quant * res[i].price;
+                console.log("Okay your total will be $" + price);
+                inquirer.prompt({
+                  name: "continue",
+                  type: "confirm",
+                  message: "would you like to continue shopping?",
+                  default: true
+                });
+                // .then(function(answer) {
+                //   console.log(answer);
+                //   if (answer == "true") {
+                //     start();
+                //   }
+                // });
+              } else {
+                console.log("Insufficient Quantity! Lets try again, friend.");
+                start();
+              }
             });
         }
       }
